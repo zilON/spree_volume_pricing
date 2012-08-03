df969f00ae3e46aad7fa031791363e8543891960
Michael Bianco <iloveitaly@gmail.com>
2012-08-03 11:13:28 -0400

    Tests are passing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,7 +9,6 @@ require 'spree/core/testing_support/factories'
 require 'spree/core/testing_support/env'
 require 'spree/core/testing_support/controller_requests'
 require 'spree/core/url_helpers'
-# require 'spree/core/testing_support/authorization_helpers'
 
 # include local factories
 Dir["#{File.dirname(__FILE__)}/factories/**/*.rb"].each do |f|
@@ -19,12 +18,12 @@ end
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
   config.mock_with :rspec
-  config.use_transactional_fixtures = false
+  config.use_transactional_fixtures = true
   config.include FactoryGirl::Syntax::Methods
   config.include Spree::Core::UrlHelpers
-  config.include Spree::Core::TestingSupport::ControllerRequests
+  config.include Spree::Core::TestingSupport::ControllerRequests, :type => :controller
 end
